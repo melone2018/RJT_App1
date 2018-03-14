4eaec438d6e8dcf70ca3a88707f4d3289e76ea91
Rui <liurui320916@gmail.com>
2018-03-14 15:21:24 -0500

    Add a variable

--- app/src/main/java/package1/RegexEample.java
+++ app/src/main/java/package1/RegexEample.java
@@ -7,6 +7,8 @@ package package1;
 import java.util.regex.*;
 class RegexExample2{
     public static void main(String args[]){
+
+        int v = 19;
         System.out.println(Pattern.matches(".s", "as"));//true (2nd char is s)
         System.out.println(Pattern.matches(".s", "mk"));//false (2nd char is not s)
         System.out.println(Pattern.matches(".s", "mst"));//false (has more than 2 char)
